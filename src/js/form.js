020a43b357bfac393893475b9883d54db2da580e
Walter Fritsche <wfritsche95@protonmail.com>
2021-07-06 15:53:21 -0500

    added a skeleton for form validation

--- src/js/form.js
+++ src/js/form.js
@@ -6,14 +6,49 @@ class Form {
     this.passElem = pass;
     this.passConfElem = passConf;
     this.submit = this.submit.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
   }
   submit(e) {
-    // e.preventDefault();
+    e.preventDefault();
+    console.log('');
     console.log(`Email: ${this.emailElem.value}`);
+    console.log(`Email is valid: ${this.validateEmail()}`);
+
     console.log(`Country: ${this.countryElem.value}`);
     console.log(`Zip: ${this.zipElem.value}`);
+    console.log(`Zip is valid: ${this.validateZip()}`);
     console.log(`Password: ${this.passElem.value}`);
     console.log(`PasswordConfirmation: ${this.passConfElem.value}`);
+    this.highfive()
+  }
+  highfive() {
+    console.log('High Five!!!');
+  }
+  validateEmail() {
+    const regex = /^[a-z0-9._-]+@[a-z0-9.]{3,}\.[a-z]{2,4}$/i;
+    const isValid = regex.test(this.emailElem.value);
+    const parentElement = this.emailElem.parentElement;
+    const hasErrorMsg = parentElement.lastElementChild.classList.contains('email-error-msg');
+    if (!isValid) {
+      if (hasErrorMsg == false) {
+        const errorMsg = document.createElement('p');
+        errorMsg.classList.add('email-error-msg');
+        errorMsg.classList.add('form-error-msg');
+        errorMsg.textContent = "Your email is not in the right formating: example@email.com!";
+        parentElement.appendChild(errorMsg);
+      }
+    } else {
+      if (hasErrorMsg) {
+        parentElement.lastElementChild.remove();
+      }
+    }
+    return isValid;
+  }
+  validateZip() {
+    return this.zipElem.value.toString().length == 5;
+  }
+  validatePassword() {
+
   }
 }
 
